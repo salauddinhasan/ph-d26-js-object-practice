c00eb2806b17a0329a97c267f6ec1b1456bd549b
salauddin <salauddin@hasan.com>
2026-01-28 11:11:01 +0600

    Add passenger capacity property to car object

---
+++ task-3.js
@@ -0,0 +1,13 @@
+// Task-2
+// For this object below add a property named passenger capacity with value 5
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+car.passenger = 5;
+console.log(car);
+car['passenger capacity'] = 10;
+console.log(car);
