c12fa7ebb438685115b8c8545269b3947b66d040
salauddin <salauddin@hasan.com>
2026-01-28 11:22:50 +0600

    Count the number of properties in student object

---
+++ task-4.js
@@ -0,0 +1,21 @@
+// Task-4
+// Count the number of properties.
+
+// Input:
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+
+// option 1
+//  Object.keys(student).length
+console.log(Object.keys(student).length);
+
+
+// option 2
+let properties = Object.keys(student).length;
+console.log(properties);
